1f10b437d59564fadbfaca9a40ef6adeac740d8a
Heinz N. Gies <heinz@licenser.net>
2012-12-09 20:17:32 +0100

    Branching out printf.

--- c_src/erldtrace.c
+++ c_src/erldtrace.c
@@ -243,10 +243,10 @@ chewrec(const dtrace_probedata_t *data, const dtrace_recdesc_t *rec, void *arg)
     if (rec->dtrd_action == DTRACEACT_PRINTF ||
 	rec->dtrd_action == DTRACEACT_PRINTA) {
 #ifdef DEBUG
-    fprintf(stderr, "chewrec - next for printf.\n\r");
+      fprintf(stderr, "chewrec - next for printf.\n\r");
 #endif
-    return (DTRACE_CONSUME_NEXT);
-  }
+      return (DTRACE_CONSUME_THIS);
+    }
 #ifdef DEBUG
     fprintf(stderr, "chewrec - invalid record: %d\n\r", rec->dtrd_action);
 #endif
