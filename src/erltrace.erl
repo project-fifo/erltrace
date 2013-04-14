9ae2172fe24f930a1e0924686ea5880d031eb828
Heinz N. Gies <heinz@licenser.net>
2013-04-14 06:04:55 +0200

    Fixed bad priv dir link.

--- src/erltrace.erl
+++ src/erltrace.erl
@@ -1,14 +1,14 @@
 -module(erltrace).
 
 -export([open/0,
-	 script/1,
-	 setopt/3,
-	 compile/2,
-	 go/1,
-	 stop/1,
-	 consume/1,
-	 walk/1
-	]).
+         script/1,
+         setopt/3,
+         compile/2,
+         go/1,
+         stop/1,
+         consume/1,
+         walk/1
+        ]).
 
 -on_load(init/0).
 
@@ -17,9 +17,9 @@
 -type dtrace_error() :: {error, {dtrace, string()}}.
 
 -type agr_type() :: count |
-		    min | max |
-		    sum | avg |
-		    quantize | lquantize | llquantize.
+                    min | max |
+                    sum | avg |
+                    quantize | lquantize | llquantize.
 
 
 -type walk_rec() :: {agr_type(), probe_rec(), number()}.
@@ -31,23 +31,23 @@
 -type probe_rec() :: term().
 
 -type probe() :: {probe, probe_desc()} |
-		 {probe, probe_desc(), probe_rec()}.
+                 {probe, probe_desc(), probe_rec()}.
 
 -type dtrace_consume_data() :: [probe()].
 
 init() ->
     PrivDir = case code:priv_dir(?MODULE) of
-        {error, _} ->
-            EbinDir = filename:dirname(code:which(?MODULE)),
-            AppPath = filename:dirname(EbinDir),
-            filename:join(AppPath, "priv");
-        Path ->
-            Path
-    end,
+                  {error, _} ->
+                      EbinDir = filename:dirname(code:which(?MODULE)),
+                      AppPath = filename:dirname(EbinDir),
+                      filename:join(AppPath, "priv");
+                  Path ->
+                      Path
+              end,
     erlang:load_nif(filename:join(PrivDir, "erltrace_drv"), 0).
 
 -spec script(Script::string()) -> {ok, Handle::dtrace_handle()} |
-		dtrace_error().
+                                  dtrace_error().
 
 script(Script) ->
     {ok, H} = open(),
@@ -55,52 +55,52 @@ script(Script) ->
     {ok, H}.
 
 -spec open() -> {ok, Handle::dtrace_handle()} |
-		dtrace_error().
+                dtrace_error().
 open() ->
     exit(nif_library_not_loaded).
 
 -spec setopt(Handle::dtrace_handle(),
-	     Option::string(),
-	     Value::string()) -> ok |
-				 dtrace_error().
+             Option::string(),
+             Value::string()) -> ok |
+                                 dtrace_error().
 
 setopt(_Handle, _Opt, _Value) ->
     exit(nif_library_not_loaded).
 
 
 -spec compile(Handle::dtrace_handle(),
-	      Script::string()) -> ok |
-				   {error, already_running} |
-				   dtrace_error().
+              Script::string()) -> ok |
+                                   {error, already_running} |
+                                   dtrace_error().
 
 compile(_Handle, _Script) ->
     exit(nif_library_not_loaded).
 
 
 -spec go(Handle::dtrace_handle()) -> ok |
-				     {error, no_prog}.
+                                     {error, no_prog}.
 
 go(_Handle) ->
     exit(nif_library_not_loaded).
 
 -spec stop(Handle::dtrace_handle()) -> ok |
-				       {error, no_prog}.
+                                       {error, no_prog}.
 
 stop(_Handle) ->
     exit(nif_library_not_loaded).
 
 -spec consume(Handle::dtrace_handle()) -> ok |
-					  {ok, dtrace_consume_data()} |
-					  dtrace_error() |
-					  {error, no_prog}.
+                                          {ok, dtrace_consume_data()} |
+                                          dtrace_error() |
+                                          {error, no_prog}.
 
 consume(_Handle) ->
     exit(nif_library_not_loaded).
 
 -spec walk(Handle::dtrace_handle()) -> ok |
-				       {ok, dtrace_walk_data()} |
-				       dtrace_error() |
-				       {error, exited | filled | stopped | no_prog}.
+                                       {ok, dtrace_walk_data()} |
+                                       dtrace_error() |
+                                       {error, exited | filled | stopped | no_prog}.
 
 walk(_Handle) ->
     exit(nif_library_not_loaded).
